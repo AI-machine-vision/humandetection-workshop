8725c13c173a764a7fb30e6e7b1c0ae946f66a34
champ-ps <pornthep.sarakon@gmail.com>
2022-09-07 22:38:27 +0700

    add setup env

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1x6saKGM_kCY_7q1TrACQ2xPoNH9xJPtV' -O 'Regression and Classification (Friday).pdf'
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1MvInYm49rp7IuSDHjwpjVQnd5zKSwsFH' -O 'Image processing and Deep learning (Saturday).pdf'
+wget -P --no-check-certificate '3_Image processing workshop/images' 'https://docs.google.com/uc?export=download&id=1T1KGnW1sxHtgaUmGJ7Fx-pCpFOU7bnDw' -O input.mp4
+wget -P '4_Deep learning workshop/checkpoints' 'https://github.com/500swapnil/Keras_Efficientnet_SSD/releases/download/v1.0/efficientnetb0_SSD.h5'
 pip install scikit-learn efficientnet
