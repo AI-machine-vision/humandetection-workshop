8cf0df11965eaf017c57b517a133475d51b17acf
champ-ps <pornthep.sarakon@gmail.com>
2022-09-08 19:59:56 +0700

    add pip install

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1x6sa
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1MvInYm49rp7IuSDHjwpjVQnd5zKSwsFH' -O 'Image processing and Deep learning (Saturday).pdf'
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1T1KGnW1sxHtgaUmGJ7Fx-pCpFOU7bnDw' -O '3_Image processing workshop/images/input.mp4'
 wget -P '4_Deep learning workshop/checkpoints' 'https://github.com/500swapnil/Keras_Efficientnet_SSD/releases/download/v1.0/efficientnetb0_SSD.h5'
-pip install scikit-learn efficientnet tensorflow_datasets
+pip install scikit-learn efficientnet tensorflow tensorflow_datasets
+pip install pandas matplotlib opencv_python
